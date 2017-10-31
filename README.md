cded3a2148d7e95166804385c393e71a0f19db81
G4br13l4 <gabolf93@gmail.com>
2017-10-31 09:25:29 -0600

    Pseudo Code & Program (Ready)

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 ## Proyecto tarjeta de credito valida
 
-1. Pedir a usuario que ingrese numero de tarjeta de credito
+1. Pedir a usuario que ingrese número de tarjeta de crédito
 2. Verificar que el usuario no ingrese un campo vacio
 3. Mandar alert para informar que ingrese su numero de tarjeta sino continuar
 4. Guardar el numero en la variable "creditCardNumber"
-5. Declarar un arreglo vacio llamado "creditCardNumber_Array"
+5. Declarar un objeto vacio llamado "creditCardNumber_All"
 6. Meter cada elemento de la variable "creditCardNumber" en el array "creditCardNumber_Array"
 7. Invertir los elementos del array "creditCardNumber_Array" con el metodo reverse()
 8. Declarar array vacio "newArray"
 9. Crear un FOR que con un IF dentro en el cual los numeros en un indice par se multipliquen por 2 y los numeros en un indice impar se mantengan igual. 
 10. Dentro del IF, poner otra condicion de que en el caso de numero en indice par, si resultado es mayor a 9, separar digitos
-11. Guardar nuevos digitos en un array nuevo llamado "newArray"
-12. Declarar variable "sum"
-13. Convertir strings de "newArray" a numbers
-14. Sumar elementos de nuevo array con un FOR y guardar en variable "sum"
-15. Al resultado de la suma, sacar el residuo de la division entre 10 y guardar en variable
-16. Mostrar si es una tarjeta valida si su residuo es 0
-17. Mostrar que es una tarjeta invalida si su residuo no es 0
+11.	Declarar variable "newObject" vacìo
+12. Guardar nuevos digitos en un object nuevo llamado "newObject"
+13. Declarar variable "sum"
+14. Convertir strings de "newObject" a numbers
+15. Sumar elementos de nuevo object con un FOR y guardar en variable "sum"
+16. Al resultado de "sum", sacar el residuo de la division entre 10 y guardar en variable "residue"
+17. Mostrar si es una tarjeta valida si su residuo es 0
+18. Mostrar que es una tarjeta invalida si su residuo no es 0
+19. Llamar a función
 
-// Me falta integrar el return de la funcion
-// creo que a una variable le puse array y es un object XD
+
+
 
--- js/app.js
+++ js/app.js
@@ -2,73 +2,73 @@
 
 function isValidCard (creditCardNumber)                                                                 {
     
-    /*Pedir a usuario que ingrese numero de tarjeta de credito y
-    Guardar el numero en la variable "creditCardNumber"*/
+    /*Pedir a usuario que ingrese número de tarjeta de crédito y
+    Guardar el número en la variable "creditCardNumber"*/
 	creditCardNumber = prompt("Ingrese por favor su número de tarjeta de crédito"); 
     
-    //Verificar que el usuario no ingrese un campo vacio
+    //Verificar que el usuario no ingrese un campo vacío
 	if(creditCardNumber === "") {  
 	//Mandar alert para informar que ingrese su numero de tarjeta sino continuar 
   	alert("Por favor ingrese su número de tarjeta"); 
 	}
 
-	//Declarar un arreglo vacio llamado "creditCardNumber_Array"
-	var creditCardNumber_Array = [];
+	//Declarar un objeto vacío llamado "creditCardNumber_All"
+	var creditCardNumber_All = [];
 
-	//Meter cada elemento de la variable "creditCardNumber" en el array "creditCardNumber_Array"
+	//Meter cada elemento de la variable "creditCardNumber" en el array "creditCardNumber_All"
 	for(var i = 0; i < creditCardNumber.length; i++)  {	
-	creditCardNumber_Array.push(creditCardNumber.charAt(i));
+	creditCardNumber_All.push(creditCardNumber.charAt(i));
 	}
 	
-	//Invertir los elementos del array "creditCardNumber_Array" con el metodo reverse()
-	creditCardNumber_Array = creditCardNumber_Array.reverse();
-	console.log(creditCardNumber_Array);
+	//Invertir los elementos del array "creditCardNumber_All" con el metodo reverse()
+	creditCardNumber_All = creditCardNumber_All.reverse();
+	console.log(creditCardNumber_All);
 
-	/*Crear un FOR que con un IF dentro en el cual los numeros en un indice par se multipliquen por 2 y
-	Guardar nuevos digitos en un array nuevo llamado "newArray" y Declarar "newArray" vacío*/
+	/*Crear un FOR que con un IF dentro en el cual los números en un índice par se multipliquen por 2 y
+	Guardar nuevos digitos en un objeto	nuevo llamado "newObject" y Declarar "newObject" vacío*/
 
-	var newArray= [];
-	for(var j = 0; j < creditCardNumber_Array.length; j++) 					{
+	var newObject= [];
+	for(var j = 0; j < creditCardNumber_All.length; j++) 					{
 		if(j % 2 !==0)											{
-			var result = creditCardNumber_Array[j] * 2;
+			var result = creditCardNumber_All[j] * 2;
 			/*Dentro del IF, poner otra condicion de que en el caso de numero en indice par, 
 			si resultado es mayor a 9, separar digitos*/
 			if(result > 9)								 	{
 				result= result.toString();
 				for( var k = 0; k < result.length; k++){
-					newArray.push(result[k]);
+					newObject.push(result[k]);
 					}
 			} else                                    	    {
-				newArray.push(result);
+				newObject.push(result);
 				}
 		//y los numeros en un indice impar se mantengan igual
-		//Guardar nuevos digitos en un array nuevo llamado "newArray"		
+		//Guardar nuevos digitos en un object nuevo llamado "newObject"	
 		}else 														{
-			result= creditCardNumber_Array[j] * 1;
-			newArray.push(result);
+			result= creditCardNumber_All[j] * 1;
+			newObject.push(result);
 		}
 	}
 
-	//Sumar elementos de nuevo array con un FOR y guardar en variable "sum"
+	//Sumar elementos de "newObject" con un FOR y guardar en variable "sum"
 	var sum = 0;
-	for(var m = 0; m < newArray.length; m++)			{
-	//Convertir strings de "newArray" a numbers
-	sum += parseInt(newArray[m]);
+	for(var m = 0; m < newObject.length; m++)			{
+	//Convertir strings de "newObject" a numbers
+	sum += parseInt(newObject[m]);
 	}
 
 	//Al resultado de "sum", sacar el residuo de la division entre 10 y guardar en variable "residue"
 	var residue = sum % 10;
 
-	//Mostrar si es una tarjeta valida si su residuo es 0
+	//Mostrar si es una tarjeta válida si su residuo es 0
 	if(residue === 0)	{
-  		alert("Su numero de tarjeta es válido");
-		//Mostrar que es una tarjeta invalida si su residuo no es 0
+  		return(alert("Su numero de tarjeta es válido"));
+		//Mostrar que es una tarjeta inválida si su residuo no es 0
 	} else              {
-  		alert("Su numero de tarjeta es inválido");
+  		return(alert("Su numero de tarjeta es inválido"));
 	}
 }
-
-console.log(isValidCard());
+//Llamar a función
+isValidCard();
 
 
 
